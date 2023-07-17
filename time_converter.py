4180dda5f57d1a2af2157bbd2bf4de714c46a96a
flyinghorse0510 <mhy@flyinghorse.top>
2023-07-17 10:37:31 +0800

    fix time calibration issues

--- time_converter.py
+++ time_converter.py
@@ -5,7 +5,8 @@ def get_current_left_time(calibrationValue: int = 0, calibration: bool = False) 
     global calibrationTime
     currentTime = 15 - (int(time.time()) - calibrationTime) % 35
     if calibration:
-        calibrationTime = calibrationValue - currentTime
+        calibrationTime += calibrationValue - currentTime
+        calibrationTime = calibrationTime % 35
         currentTime = calibrationValue
     return currentTime
 
@@ -15,3 +16,11 @@ def get_current_unix_time() -> int:
 # while True:
 #     print(get_current_left_time())
 #     time.sleep(0.99)
+
+# while True:
+#     currentTime = get_current_left_time()
+#     if currentTime == 14:
+#         print("%d -> 10" %(currentTime))
+#         currentTime = get_current_left_time(10, True)
+#     print(get_current_left_time())
+#     time.sleep(1)
