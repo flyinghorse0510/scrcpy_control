b6639f690b24eef579407a5e7e3d840abbe9ec0d
Haoyuan Ma <me@flyinghorse.top>
2023-08-16 13:50:51 +0800

    add partial texas coordinate

--- texas.py
+++ texas.py
@@ -4,8 +4,8 @@ import numpy as np
 from multiprocessing import Process
 from multiprocessing import Queue
 from multiprocessing import Lock
-from tesserocr import PyTessBaseAPI, PSM
-from tesserocr import get_languages
+# from tesserocr import PyTessBaseAPI, PSM
+# from tesserocr import get_languages
 import queue
 import remote_control
 import random
@@ -13,8 +13,24 @@ import time
 import time_converter
 import sys
 import support_line as sline
+import utils
 
-BottomBetArea = (1114, 296, 1414, 344)
+BottomBetArea = (1084, 300, 1440, 340)
+GameBeginArea = (1106, 484, 1430, 532)
+
+PlayerStatusArea = (
+    (768, 24, 918, 68),
+    (424, 154, 574, 198),
+    (368, 484, 518, 528),
+    (658, 678, 808, 722),
+    (1192, 678, 1342, 722),
+    (1720, 678, 1870, 722),
+    (2008, 484, 2158, 528),
+    (1948, 154, 2098, 198),
+    (1608, 24, 1758, 68)
+)
+
+
 
 
 
@@ -22,7 +38,10 @@ def bottom_bet_test(imgPath: str) -> bool:
     cvImg = cv2.imread(imgPath)
     originalImg = Image.fromarray(cv2.cvtColor(cvImg, cv2.COLOR_BGR2RGB))
     bottomBetImg = originalImg.crop(BottomBetArea)
-    bottomBetImg.show()
+    bottomBetImg.save("./tmp/bottomBet.png")
+    binarizedBottomImg = utils.binarizePillow(bottomBetImg, 90)
+    binarizedBottomImg.save("./tmp/binarizedBottomBet.png")
+    # bottomBetImg.show()
     
 
 bottom_bet_test("./texas/user_thinking.png")
---
+++ utils.py
@@ -0,0 +1,56 @@
+from PIL import Image, ImageOps
+
+MAX_INT_NUMBER = 1000000000
+
+def txt2int(txt: str) -> int:
+    if len(txt) == 0:
+        return 0
+    number = 0
+    multi = 1
+    realTxt = txt
+    if txt[-1] == "万":
+        multi = 10000
+        realTxt = realTxt[:-1]
+    elif txt[-1] == "亿":
+        multi = 100000000
+        realTxt = realTxt[:-1]
+    
+    try:
+        number = int(float(realTxt) * multi + 0.5)
+    except ValueError as e1:
+        number = -1
+    except OverflowError as e2:
+        number = -1
+    except:
+        print("Convert Error!")
+        number = -1
+    
+    number = number if number < MAX_INT_NUMBER else -1
+    return number
+
+def is_chinese(txt: str) -> bool:
+    for i in range(len(txt)):
+        if txt[i] < '\u4e00' or txt[i] > '\u9fa5':
+            return False
+    return True
+
+def pureTxt2int(txt: str) -> int:
+    if len(txt) == 0:
+        return 0
+    try:
+        number = int(txt)
+    except ValueError as e:
+        number = -1
+    except:
+        print("Convert Error!")
+        number = -1
+        
+    number = number if number < MAX_INT_NUMBER else -1
+    return number
+
+def cleanStr(txt: str) -> str:
+    return txt.replace(" ", "").replace("-","").replace("一","").strip("\n")
+
+def binarizePillow(img: Image.Image, threshold: int) -> Image.Image:
+    outputImg = ImageOps.grayscale(img)
+    return outputImg.point(lambda x: 0 if x > threshold else 255)
